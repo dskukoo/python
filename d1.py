86bd484c92aca0a5052a681fdcae5afe0e4ad913
kartikey kuswah <kartikey1045@gmail.com>
2026-02-10 23:09:00 +0530

    day2

--- d1.py
+++ d1.py
@@ -1,1 +1,14 @@
+#day1
 print("kartikey kushwah")
+
+#day2
+name = "kartikey"
+age =21
+address = "101, kutiyana moholla goshpura no 1 gwalior madhya pradesh"
+
+print("name", name, "age", age,
+      "address", address
+     )
+
+
+
