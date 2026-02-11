906cd95e7e553987b3a0dc3cfbf641f6d79660cf
kartikey kuswah <kartikey1045@gmail.com>
2026-02-11 17:29:13 +0530

    day3.2

--- day3.py
+++ day3.py
@@ -6,10 +6,15 @@ print("you entered", example)*/'''
 '''data = input("Enter values").split(',')
 print("you entered", data)'''
 
-data = []
+'''data = []
 
 data.append(int(input("enter a number")))
 data.append(input("enter a name"))
-print("you entered", data)
+print("you entered", data)'''
 
+mylist = []
+mylist.append(int(input("enter a number")))
+mylist.append(int(input("enter a number")))
+sum = sum(mylist)
+print("the sum is", sum)
 
