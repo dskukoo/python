f7adc68663b48dd48bfceff347a6675d6ca8cfc4
kartikey kuswah <kartikey1045@gmail.com>
2026-03-01 13:03:00 +0530

    day 19

--- day4.py
+++ day4.py
@@ -160,11 +160,24 @@ for j in range(5):
     
 print(largest)'''
 
-ls = [1,2,3,4,5]
+'''ls = [1,2,3,4,5,]
 sum=0
 for num in ls:
     sum=sum+num
-print(sum)
+print(sum)'''
+
+
+'''my_list = [1,2,3,4,4,5]
+new_list = []
+for item in my_list:
+    if item not in new_list:
+        new_list.append(item)
+
+print(new_list)'''
+
+
+    
+
 
 
 
